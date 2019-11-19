51ef6e740ba244820d01a8dbd5dfee05726a4fc4
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-11-19 17:14:14 -0500

    created function for sections, added a table view and conformed to data source

--- GameOfThrones/GameOfThrones/ViewController.swift
+++ GameOfThrones/GameOfThrones/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var someEpisode = [GOTEpisode]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    tableView.dataSource = self
+    print("there are \(GOTEpisode.seasonSection().count) seasons.")
+   
   }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          let cell = tableView.dequeueReusableCell(withIdentifier: "animalCell", for: indexPath)
+        
+        return cell
+    }
+    
+}
